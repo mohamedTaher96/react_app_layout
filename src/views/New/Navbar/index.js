341632361a8249d2c706982390b69735ca1ef30d
ibrahimmohammed11 <ibrahimmohammed9979@gmail.com>
2021-11-01 10:09:02 +0200

    edit

--- src/views/New/Navbar/index.js
+++ src/views/New/Navbar/index.js
@@ -14,66 +14,84 @@ export default class Navbar extends Component {
   render() {
     return (
       <Fragment>
-        <nav className={`${Styles.navMar} navbar navbar-light bg-white`}>
+        <nav
+          className={`${Styles.navMar} navbar navbar-expand-md navbar-light bg-white`}
+        >
           <div className="container">
             <Link to="/" className="navbar-brand">
               <img src={logo} alt="img1" className={Styles.logoSt} />
             </Link>
-            <ul className="navbar-nav me-auto mb-2">
-              <li className="nav-item">
-                <input
-                  className={Styles.searchSt}
-                  type="text"
-                  placeholder="Search..."
-                />
-              </li>
-            </ul>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2">
+                <li className="nav-item">
+                  <input
+                    className={Styles.searchSt}
+                    type="text"
+                    placeholder="Search..."
+                  />
+                </li>
+              </ul>
 
-            <ul className="list-unstyled mr-auto d-flex justify-content-between align-items-center">
-              <li className={`${Styles.iconSt} nav-item`}>
-                <InlineSVG src={icon1} />
-              </li>
-              <li
-                className={`${Styles.iconSt} ${Styles.dropdownSt} nav-item ml-2`}
-              >
-                <InlineSVG src={icon3} />
+              <ul className="list-unstyled mr-auto d-flex justify-content-between align-items-center">
+                <li className={`${Styles.iconSt} nav-item`}>
+                  <InlineSVG src={icon1} />
+                </li>
+                <li
+                  className={`${Styles.iconSt} ${Styles.dropdownSt} nav-item ml-2`}
+                >
+                  <InlineSVG src={icon3} />
 
-                <div className={Styles.dropdownContent}>
-                  <div className="row">
-                    <div className="col-6">
-                      <Link to="/companycats">
-                        <div className={Styles.divSty}>
-                          <img src={img2} alt="company" />
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="col-6">
-                      <Link to="/companycats">
-                        <div className={Styles.divSty}>
-                          <img src={img2} alt="company" />
-                        </div>
-                      </Link>
-                    </div>
-                    <div className="col-6">
-                      <Link to="/companycats">
-                        <div className={Styles.divSty}>
-                          <i className="fas fa-plus-circle"></i>
-                          <span>Add Company</span>
-                        </div>
-                      </Link>
+                  <div className={Styles.dropdownContent}>
+                    <div className="row">
+                      <div className="col-6">
+                        <Link to="/companycats">
+                          <div className={Styles.divSty}>
+                            <img src={img2} alt="company" />
+                          </div>
+                        </Link>
+                      </div>
+                      <div className="col-6">
+                        <Link to="/companycats">
+                          <div className={Styles.divSty}>
+                            <img src={img2} alt="company" />
+                          </div>
+                        </Link>
+                      </div>
+                      <div className="col-6">
+                        <Link to="/companycats">
+                          <div className={Styles.divSty}>
+                            <i className="fas fa-plus-circle"></i>
+                            <span>Add Company</span>
+                          </div>
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-              <li className={`${Styles.iconSt} nav-item mx-2`}>
-                <InlineSVG src={icon2} />
-                <i className={`${Styles.greenDot1} fas fa-circle`}></i>
-              </li>
-              <li className={`${Styles.profImg} nav-item `}>
-                <img src={profImg} alt="img1" />
-                <i className={`${Styles.greenDot2} fas fa-circle`}></i>
-              </li>
-            </ul>
+                </li>
+                <li className={`${Styles.iconSt} nav-item mx-2`}>
+                  <InlineSVG src={icon2} />
+                  <i className={`${Styles.greenDot1} fas fa-circle`}></i>
+                </li>
+                <li className={`${Styles.profImg} nav-item `}>
+                  <img src={profImg} alt="img1" />
+                  <i className={`${Styles.greenDot2} fas fa-circle`}></i>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
       </Fragment>
